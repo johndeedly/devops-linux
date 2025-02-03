02526b9a41fe9c3560af687c7bd9c5eaf4627ac8
johndeedly <johndeedly@github.com>
2025-02-03 17:49:02 +0100

    podman setup for apt and pacman

---
+++ config/apt/20_podman_apt.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+exec &> >(while IFS=$'\r' read -ra line; do [ -z "${line[@]}" ] && line=( '' ); TS=$(</proc/uptime); echo -e "[${TS% *}] ${line[-1]}" | tee -a /cidata_log > /dev/tty1; done)
+
+LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata apt -y install podman-docker podman-compose docker-compose fuse-overlayfs \
+    btrfs-progs portainer-bin cockpit-podman
+
+# Enable all configured services
+systemctl enable podman portainer
+
+firewall-offline-cmd --zone=public --add-port=8000/tcp
+firewall-offline-cmd --zone=public --add-port=9000/tcp
+firewall-offline-cmd --zone=public --add-port=9443/tcp
+
+# sync everything to disk
+sync
+
+# cleanup
+rm -- "${0}"
