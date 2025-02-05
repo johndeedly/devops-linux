0d2bc9f16548719b605ce8e2eb468b7fee26c5e8
johndeedly <johndeedly@github.com>
2025-02-05 03:53:43 +0100

    error fix for "unexpected redirect": no output redirection this early in the process

--- config/archiso/bootup.sh
+++ config/archiso/bootup.sh
@@ -1,8 +1,6 @@
 #cloud-boothook
 #!/usr/bin/env bash
 
-exec &> >(while IFS=$'\r' read -ra line; do [ -z "${line[@]}" ] && line=( '' ); TS=$(</proc/uptime); echo -e "[${TS% *}] ${line[-1]}" | tee -a /cidata_log > /dev/tty1; done)
-
 # stop and disable reflector to be able to change the pacman mirrorlist
 # run pacman init when everything is ready
 systemctl stop reflector.service reflector.timer pacman-init.service
