165963dff2763f50cc60664dc8964ae9f122f5c3
johndeedly <johndeedly@github.com>
2025-02-20 04:21:31 +0100

    gitlab docker proof of concept

---
+++ config/podman/40_gitlab_podman.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+exec &> >(while IFS=$'\r' read -ra line; do [ -z "${line[@]}" ] && line=( '' ); TS=$(</proc/uptime); echo -e "[${TS% *}] ${line[-1]}" | tee -a /cidata_log > /dev/tty1; done)
+
+# enable and start gitlab
+PROJECTNAME="gitlab"
+TMPDIR="$(mktemp -d)"
+BUILDTMP="${TMPDIR}/${PROJECTNAME}"
+mkdir -p "${BUILDTMP}"
+
+tee "${BUILDTMP}/podman-compose.yml" <<EOF
+name: ${PROJECTNAME}
+networks:
+  lan:
+volumes:
+  config:
+  logs:
+  data:
+services:
+  main:
+    image: gitlab/gitlab-ce:latest
+    restart: unless-stopped
+    networks:
+      - lan
+    ports:
+      - '8929:80'
+      - '2424:22'
+    environment:
+      GITLAB_OMNIBUS_CONFIG: |
+        external_url 'http://gitlab.example.com:8929'
+        gitlab_rails['gitlab_shell_ssh_port'] = 2424
+    volumes:
+      - config:/etc/gitlab
+      - logs:/var/log/gitlab
+      - data:/var/opt/gitlab
+EOF
+pushd "${BUILDTMP}"
+  podman-compose up --no-start
+popd
+pushd /etc/systemd/system
+  podman generate systemd --new --name "${PROJECTNAME}_main_1" -f
+popd
+systemctl enable "container-${PROJECTNAME}_main_1"
+
+firewall-offline-cmd --zone=public --add-port=8929/tcp
+
+# sync everything to disk
+sync
+
+# cleanup
+rm -r "${TMPDIR}"
+rm -- "${0}"
