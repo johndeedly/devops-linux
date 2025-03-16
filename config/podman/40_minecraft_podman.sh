63b5a4add46bee527347683c4ce0e9a82af8629a
johndeedly <johndeedly@github.com>
2025-03-16 06:42:01 +0100

    improvement: switched to more modern datapack versions of minecraft mods

--- config/podman/40_minecraft_podman.sh
+++ config/podman/40_minecraft_podman.sh
@@ -32,18 +32,20 @@ services:
       MEMORY: 2G
       MAX_PLAYERS: 5
       MOTD: Local Minecraft Create server
-      PLUGINS: "https://cdn.modrinth.com/data/LNytGWDc/versions/IJpm7znS/create-1.21.1-6.0.1.jar\n
+      MODS: "https://cdn.modrinth.com/data/LNytGWDc/versions/IJpm7znS/create-1.21.1-6.0.1.jar\n
         https://cdn.modrinth.com/data/hSSqdyU1/versions/k0XmHBh4/Create%20Encased-1.21.1-1.7.0.jar\n
         https://cdn.modrinth.com/data/9enMEvoc/versions/cBxRUPWE/create_structures_arise-147.20.19-neoforge-1.21.1.jar\n
-        
-        https://cdn.modrinth.com/data/tpehi7ww/versions/BYUUUeZA/dungeons-and-taverns-v4.4.4.jar\n
-        https://cdn.modrinth.com/data/8oi3bsk5/versions/MuJMtPGQ/Terralith_1.21.x_v2.5.8.jar\n
-        https://cdn.modrinth.com/data/ZVzW5oNS/versions/pwe1kTJE/Incendium_1.21_UNSUPPORTED_PORT_v5.4.4.zip\n
-        https://cdn.modrinth.com/data/LPjGiSO4/versions/dHJAVX8s/Nullscape_1.21.x_v1.2.10.jar\n
-        
-        https://cdn.modrinth.com/data/wKkoqHrH/versions/3rUDJIS0/geyser-neoforge-Geyser-Neoforge-2.4.4-b705.jar\n
-        https://cdn.modrinth.com/data/bWrNNfkb/versions/YapRHgnZ/Floodgate-Neoforge-2.2.4-b38.jar\n
         https://cdn.modrinth.com/data/MPCX6s5C/versions/uZ2kVr2B/notenoughanimations-neoforge-1.9.2-mc1.21.jar"
+      PLUGINS: "https://cdn.modrinth.com/data/9eGKb6K1/versions/L9ZSz77F/voicechat-neoforge-1.21.1-2.5.26.jar\n
+        https://cdn.modrinth.com/data/wKkoqHrH/versions/3rUDJIS0/geyser-neoforge-Geyser-Neoforge-2.4.4-b705.jar\n
+        https://cdn.modrinth.com/data/bWrNNfkb/versions/YapRHgnZ/Floodgate-Neoforge-2.2.4-b38.jar"
+      DATAPACKS: "https://cdn.modrinth.com/data/OhduvhIc/versions/8cEJouzY/Veinminer-1.2.4.zip\n
+        https://cdn.modrinth.com/data/4sP0LXxp/versions/3D1S0vgH/Veinminer-Enchantment-1.2.3.zip\n
+        https://cdn.modrinth.com/data/tpehi7ww/versions/9Dw6hgJA/Dungeons%20and%20Taverns%20v4.4.4.zip\n
+        https://cdn.modrinth.com/data/8oi3bsk5/versions/urbokcOc/Terralith_1.21_v2.5.8.zip\n
+        https://cdn.modrinth.com/data/ZVzW5oNS/versions/pwe1kTJE/Incendium_1.21_UNSUPPORTED_PORT_v5.4.4.zip\n
+        https://cdn.modrinth.com/data/LPjGiSO4/versions/J4B2BaWk/Nullscape_1.21_v1.2.10.zip\n
+        https://cdn.modrinth.com/data/7YjclEGc/versions/yCdCPMZa/dynamiclights-v1.8.5-mc1.17x-1.21x-datapack.zip"
     volumes:
       - mc-data:/data
 EOF
