7077bf484c73ca5bc72ff50689d55c7c4ac05932
johndeedly <johndeedly@github.com>
2025-06-29 18:16:54 +0200

    refactoring proxmox setup

--- config/apt/20_proxmox_1_apt.sh
+++ config/apt/20_proxmox_1_apt.sh
@@ -7,20 +7,22 @@ fi
 
 exec &> >(while IFS=$'\r' read -ra line; do [ -z "${line[@]}" ] && line=( '' ); TS=$(</proc/uptime); echo -e "[${TS% *}] ${line[-1]}" | tee -a /cidata_log > /dev/tty1; done)
 
+# https://www.tecmint.com/install-proxmox/
+LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata apt install curl software-properties-common apt-transport-https ca-certificates gnupg2
+
+# install the proxmox repository key
+echo ":: download proxmox repository certificate"
+curl -fsSL https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+
 # add the proxmox repository to the package sources
 tee /etc/apt/sources.list.d/pve-install-repo.list <<EOF
 deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription
 EOF
 
-# verify and install the proxmox repository key
-echo ":: download proxmox repository certificate"
-wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
-CHECKSUM=$(sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg | cut -d ' ' -f1)
-TARGET="7da6fe34168adc6e479327ba517796d4702fa2f8b4f0a9833f5ea6e6b48f6507a6da403a274fe201595edc86a84463d50383d07f64bdde2e3658108db7d6dc87"
-if [ "$TARGET" != "$CHECKSUM" ]; then
-    echo "!! checksum mismatch"
-    exit 1
-fi
+# removes the nagging "subscription missing" popup on login (permanent solution)
+tee /etc/apt/apt.conf.d/90-no-more-nagging <<EOF
+DPkg::Post-Invoke { "/usr/bin/sed -Ezi 's/function\(orig_cmd\) \{/function\(original_cmd\) \{ original_cmd\(\); return;/g' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js"; };
+EOF
 
 # update and upgrade
 LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive apt update
@@ -30,11 +32,6 @@ LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata apt install ifu
 systemctl stop networking || true
 LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata apt install --reinstall ifupdown2
 LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata dpkg --configure -a
-
-# https://www.tecmint.com/install-proxmox/
-# as of today, ifupdown2 will fix itself here in a silent and wonderous way
-# might break in the future, of course, most probably
-LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata apt install curl software-properties-common apt-transport-https ca-certificates gnupg2
 
 # install proxmox default kernel
 LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata apt install proxmox-default-kernel
@@ -117,9 +114,6 @@ systemctl mask NetworkManager-wait-online
 
 # open up the port for the proxmox webinterface
 firewall-offline-cmd --zone=public --add-port=8006/tcp
-
-# removes the nagging "subscription missing" popup on login (no permanent solution)
-sed -Ezi 's/(function\(orig_cmd\) \{)/\1\n\torig_cmd\(\);\n\treturn;/g' /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js
 
 # sync everything to disk
 sync
