dd2c38377792f53281a0d7f9c690ca0df73f6b8f
johndeedly <johndeedly@github.com>
2025-06-29 18:16:54 +0200

    improving cicd docker by starting a nginx to serve the isos

--- config/podman/60_cicd_podman.sh
+++ config/podman/60_cicd_podman.sh
@@ -14,12 +14,17 @@ mkdir "${BUILDTMP}/cicd"
 tee "${BUILDTMP}/cicd/run-build.sh" <<'EOF'
 #!/bin/bash
 
-LC_ALL=C yes | LC_ALL=C pacman -Syu --noconfirm --needed git yq xorriso moreutils cloud-image-utils
+LC_ALL=C yes | LC_ALL=C pacman -Syu --noconfirm --needed findutils git yq xorriso moreutils cloud-image-utils
 
 git clone https://github.com/johndeedly/devops-linux.git
 
 pushd devops-linux
   LOGCOUNT=$(git log --oneline | wc -l)
+
+  # cleanup
+  find /deploy -name "*.iso" -print | while read -r line; do
+    rm "$line"
+  done
 
   # archlinux base
   yq -y '(.setup.distro) = "archlinux"' config/setup.yml | sponge config/setup.yml
@@ -53,52 +58,84 @@ pushd devops-linux
     mv archlinux-x86_64-cidata.iso "/deploy/archlinux-kde-pxe-x86_64-r${LOGCOUNT}.iso"
   fi
 
-  # debian base
-  yq -y '(.setup.distro) = "debian"' config/setup.yml | sponge config/setup.yml
+  # debian 12 base
+  yq -y '(.setup.distro) = "debian-12"' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.options) = ["base"]' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
   if ./cidata.sh --archiso; then
-    mv archlinux-x86_64-cidata.iso "/deploy/debian-base-x86_64-r${LOGCOUNT}.iso"
+    mv archlinux-x86_64-cidata.iso "/deploy/debian-12-base-x86_64-r${LOGCOUNT}.iso"
   fi
 
-  # debian mirror
-  yq -y '(.setup.distro) = "debian"' config/setup.yml | sponge config/setup.yml
+  # debian 12 mirror
+  yq -y '(.setup.distro) = "debian-12"' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.options) = ["mirror"]' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
   if ./cidata.sh --archiso; then
-    mv archlinux-x86_64-cidata.iso "/deploy/debian-mirror-x86_64-r${LOGCOUNT}.iso"
+    mv archlinux-x86_64-cidata.iso "/deploy/debian-12-mirror-x86_64-r${LOGCOUNT}.iso"
   fi
-  
-  # debian kde
-  yq -y '(.setup.distro) = "debian"' config/setup.yml | sponge config/setup.yml
+
+  # debian 12 kde
+  yq -y '(.setup.distro) = "debian-12"' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.options) = ["base","kde"]' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
   if ./cidata.sh --archiso; then
-    mv archlinux-x86_64-cidata.iso "/deploy/debian-kde-x86_64-r${LOGCOUNT}.iso"
+    mv archlinux-x86_64-cidata.iso "/deploy/debian-12-kde-x86_64-r${LOGCOUNT}.iso"
   fi
 
-  # debian kde pxe-image
-  yq -y '(.setup.distro) = "debian"' config/setup.yml | sponge config/setup.yml
+  # debian 12 kde pxe-image
+  yq -y '(.setup.distro) = "debian-12"' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.options) = ["base","kde","pxe-image"]' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
   if ./cidata.sh --archiso; then
-    mv archlinux-x86_64-cidata.iso "/deploy/debian-kde-pxe-x86_64-r${LOGCOUNT}.iso"
+    mv archlinux-x86_64-cidata.iso "/deploy/debian-12-kde-pxe-x86_64-r${LOGCOUNT}.iso"
   fi
 
-  # debian proxmox
-  yq -y '(.setup.distro) = "debian"' config/setup.yml | sponge config/setup.yml
+  # debian 12 proxmox
+  yq -y '(.setup.distro) = "debian-12"' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.options) = ["base","proxmox","proxmox-devops"]' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
   if ./cidata.sh --archiso; then
-    mv archlinux-x86_64-cidata.iso "/deploy/debian-proxmox-x86_64-r${LOGCOUNT}.iso"
+    mv archlinux-x86_64-cidata.iso "/deploy/debian-12-proxmox-x86_64-r${LOGCOUNT}.iso"
   fi
 
-  # debian podman
-  yq -y '(.setup.distro) = "debian"' config/setup.yml | sponge config/setup.yml
+  # debian 12 podman
+  yq -y '(.setup.distro) = "debian-12"' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.options) = ["base","podman"]' config/setup.yml | sponge config/setup.yml
   yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
   if ./cidata.sh --archiso; then
-    mv archlinux-x86_64-cidata.iso "/deploy/debian-podman-x86_64-r${LOGCOUNT}.iso"
+    mv archlinux-x86_64-cidata.iso "/deploy/debian-12-podman-x86_64-r${LOGCOUNT}.iso"
+  fi
+
+  # ubuntu 24 base
+  yq -y '(.setup.distro) = "ubuntu-24"' config/setup.yml | sponge config/setup.yml
+  yq -y '(.setup.options) = ["base"]' config/setup.yml | sponge config/setup.yml
+  yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
+  if ./cidata.sh --archiso; then
+    mv archlinux-x86_64-cidata.iso "/deploy/ubuntu-24-base-x86_64-r${LOGCOUNT}.iso"
+  fi
+
+  # ubuntu 24 mirror
+  yq -y '(.setup.distro) = "ubuntu-24"' config/setup.yml | sponge config/setup.yml
+  yq -y '(.setup.options) = ["mirror"]' config/setup.yml | sponge config/setup.yml
+  yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
+  if ./cidata.sh --archiso; then
+    mv archlinux-x86_64-cidata.iso "/deploy/ubuntu-24-mirror-x86_64-r${LOGCOUNT}.iso"
+  fi
+
+  # ubuntu 24 kde
+  yq -y '(.setup.distro) = "ubuntu-24"' config/setup.yml | sponge config/setup.yml
+  yq -y '(.setup.options) = ["base","kde"]' config/setup.yml | sponge config/setup.yml
+  yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
+  if ./cidata.sh --archiso; then
+    mv archlinux-x86_64-cidata.iso "/deploy/ubuntu-24-kde-x86_64-r${LOGCOUNT}.iso"
+  fi
+
+  # ubuntu 24 kde pxe-image
+  yq -y '(.setup.distro) = "ubuntu-24"' config/setup.yml | sponge config/setup.yml
+  yq -y '(.setup.options) = ["base","kde","pxe-image"]' config/setup.yml | sponge config/setup.yml
+  yq -y '(.setup.target) = "auto"' config/setup.yml | sponge config/setup.yml
+  if ./cidata.sh --archiso; then
+    mv archlinux-x86_64-cidata.iso "/deploy/ubuntu-24-kde-pxe-x86_64-r${LOGCOUNT}.iso"
   fi
 popd
 
@@ -115,6 +152,13 @@ VOLUME /deploy
 CMD ["/app/run-build.sh"]
 EOF
 
+mkdir "${BUILDTMP}/nginx"
+tee "${BUILDTMP}/nginx/Dockerfile" <<'EOF'
+FROM nginx
+
+RUN sed -i 's/index.*;/autoindex on; autoindex_exact_size off; autoindex_format html;/g' /etc/nginx/conf.d/default.conf
+EOF
+
 tee "${BUILDTMP}/podman-compose.yml" <<EOF
 name: ${PROJECTNAME}
 networks:
@@ -129,22 +173,35 @@ services:
       - 'no-new-privileges:true'
     volumes:
       - /deploy:/deploy
+  web:
+    build: ./nginx
+    restart: on-failure
+    networks:
+      - lan
+    security_opt:
+      - 'no-new-privileges:true'
+    ports:
+      - '6080:80'
+    volumes:
+      - /deploy:/usr/share/nginx/html:ro
 EOF
 pushd "${BUILDTMP}"
   podman-compose up --no-start
 popd
 pushd /etc/systemd/system
   podman generate systemd --new --name "${PROJECTNAME}_main_1" -f
+  podman generate systemd --new --name "${PROJECTNAME}_web_1" -f
 popd
 systemctl enable "container-${PROJECTNAME}_main_1"
+systemctl enable "container-${PROJECTNAME}_web_1"
 
-# daily execution
+# weekly execution
 tee "/etc/systemd/system/container-${PROJECTNAME}_main_1.timer" <<EOF
 [Unit]
-Description=Daily CI/CD job
+Description=Weekly CI/CD job
 
 [Timer]
-OnCalendar=daily
+OnCalendar=weekly
 AccuracySec=12h
 Persistent=true
 
@@ -152,6 +209,8 @@ Persistent=true
 WantedBy=timers.target
 EOF
 systemctl enable "container-${PROJECTNAME}_main_1.timer"
+
+firewall-offline-cmd --zone=public --add-port=6080/tcp
 
 # sync everything to disk
 sync
