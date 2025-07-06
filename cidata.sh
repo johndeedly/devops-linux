dd7e323d054ea13348918f02d4703c44670d91d9
johndeedly <johndeedly@github.com>
2025-07-06 15:19:36 +0200

    ldap auth configured through setup.yml

--- cidata.sh
+++ cidata.sh
@@ -150,6 +150,7 @@ write_mime_params=(
     "config/stage/i18n.yml:text/cloud-config"
     "config/stage/user-skeleton.yml:text/cloud-config"
     "config/stage/10_firstboot.sh:text/x-shellscript"
+    "config/stage/18_ldap.sh:text/x-shellscript"
     "config/stage/90_second_stage.sh:text/x-shellscript"
     "config/stage/90_final_stage.sh:application/x-per-boot"
     "config/setup.yml:application/x-setup-config"
--- config/stage/18_ldap.sh
+++ config/stage/18_ldap.sh
@@ -2,21 +2,31 @@
 
 exec &> >(while IFS=$'\r' read -ra line; do [ -z "${line[@]}" ] && line=( '' ); TS=$(</proc/uptime); echo -e "[${TS% *}] ${line[-1]}" | tee -a /cidata_log > /dev/tty1; done)
 
-LC_ALL=C yes | LC_ALL=C pacman -S --noconfirm --needed openldap xkcdpass firewalld
+LDAP_ENABLED="$(yq -r '.setup.ldapauth.enabled' /var/lib/cloud/instance/config/setup.yml)"
+LDAP_AUTHSERVER="$(yq -r '.setup.ldapauth.authserver' /var/lib/cloud/instance/config/setup.yml)"
+LDAP_BASE="$(yq -r '.setup.ldapauth.base' /var/lib/cloud/instance/config/setup.yml)"
+LDAP_GROUP="$(yq -r '.setup.ldapauth.group' /var/lib/cloud/instance/config/setup.yml)"
+LDAP_PASSWD="$(yq -r '.setup.ldapauth.passwd' /var/lib/cloud/instance/config/setup.yml)"
+LDAP_SHADOW="$(yq -r '.setup.ldapauth.shadow' /var/lib/cloud/instance/config/setup.yml)"
+
+if [ -z "$LDAP_ENABLED" ] || [[ "$LDAP_ENABLED" =~ [Nn][Oo] ]] || [[ "$LDAP_ENABLED" =~ [Oo][Ff][Ff] ]] || [[ "$LDAP_ENABLED" =~ [Ff][Aa][Ll][Ss][Ee] ]]
+then
+  sync
+  rm -- "${0}"
+  exit 0
+fi
 
 # install basic packages
 if [ -e /bin/apt ]; then
   LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata apt -y install \
-    libnss-ldap libpam-ldap ldap-utils nslcd
+    libnss-ldap libpam-ldap ldap-utils nslcd xkcdpass firewalld
 elif [ -e /bin/pacman ]; then
   LC_ALL=C yes | LC_ALL=C pacman -S --noconfirm --needed \
-    openldap nss-pam-ldapd
+    openldap nss-pam-ldapd xkcdpass firewalld
 elif [ -e /bin/yum ]; then
   LC_ALL=C yes | LC_ALL=C yum install -y \
-    openldap openldap-clients nss-pam-ldapd
+    openldap openldap-clients nss-pam-ldapd xkcdpass firewalld
 fi
-
-AUTHSERVER="0.0.0.0"
 
 echo ':: enable optional ldap pam and nss authentication'
 if [ -f /etc/pam.d/system-auth ]; then
@@ -39,8 +49,8 @@ if [ -f /etc/pam.d/su-l ]; then
 fi
 if [ -f /etc/nslcd.conf ]; then
   chmod 0600 /etc/nslcd.conf
-  sed -i "s|^uri .*|uri ldap://${AUTHSERVER}/|" /etc/nslcd.conf
-  sed -i 's|^base .*|base   dc=internal\nbase   group  ou=Groups,dc=internal\nbase   passwd ou=People,dc=internal\nbase   shadow ou=People,dc=internal|' /etc/nslcd.conf
+  sed -i "s|^\(uri .*\)|#\1\nuri ${LDAP_AUTHSERVER}|" /etc/nslcd.conf
+  sed -i "s|^\(base .*\)|#\1\nbase   ${LDAP_BASE}\nbase   group  ${LDAP_GROUP}\nbase   passwd ${LDAP_PASSWD}\nbase   shadow ${LDAP_SHADOW}|" /etc/nslcd.conf
 fi
 if [ -f /etc/nsswitch.conf ]; then
     sed -i 's/^\(passwd.*\)/\1 ldap/' /etc/nsswitch.conf
@@ -50,8 +60,8 @@ fi
 if [ -f /etc/openldap/ldap.conf ]; then
   tee -a /etc/openldap/ldap.conf <<EOF
 
-BASE    dc=internal
-URI     ldap://${AUTHSERVER}/
+BASE    ${LDAP_BASE}
+URI     ${LDAP_AUTHSERVER}
 EOF
 fi
 if [ -f /etc/pam.d/sudo ]; then
