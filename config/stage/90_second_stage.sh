86215736c86609314d9845a566b7e61a93fa0f6b
johndeedly <johndeedly@github.com>
2025-07-16 20:02:19 +0200

    more verbose second stage activation

--- config/stage/90_second_stage.sh
+++ config/stage/90_second_stage.sh
@@ -3,7 +3,14 @@
 exec &> >(while IFS=$'\r' read -ra line; do [ -z "${line[@]}" ] && line=( '' ); TS=$(</proc/uptime); echo -e "[${TS% *}] ${line[-1]}" | tee -a /cidata_log > /dev/tty1; done)
 
 # activate second stage
-find /var/lib/cloud/scripts/per-boot -name '*.sh' -exec chmod 0755 {} \;
+FILELIST=$(find /var/lib/cloud/scripts/per-boot -name '*.sh')
+if [ -n "$FILELIST" ]; then
+  echo ":: Enable second stage"
+  sort <<<"$FILELIST" | while read -r line; do
+    echo "-> $line"
+    chmod 0755 "$line"
+  done
+fi
 
 # sync everything to disk
 sync
