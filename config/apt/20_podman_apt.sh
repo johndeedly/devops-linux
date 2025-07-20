d1b2ebc95e4f0f0262c4341106a8f3379e28c635
johndeedly <johndeedly@github.com>
2025-07-20 08:40:30 +0200

    bugfix: allow traffic forwarding to containers

--- config/apt/20_podman_apt.sh
+++ config/apt/20_podman_apt.sh
@@ -16,6 +16,16 @@ EOF
 # Enable all configured services
 systemctl enable podman.service podman.socket
 
+# allow forwarding to all private networks
+ufw disable
+ufw route allow from any to 10.0.0.0/8 comment 'allow fwd to private net'
+ufw route allow from any to 172.16.0.0/12 comment 'allow fwd to private net'
+ufw route allow from any to 192.168.0.0/16 comment 'allow fwd to private net'
+ufw route allow from any to fe80::/64 comment 'allow fwd to private net'
+ufw route allow from any to fc00::/7 comment 'allow fwd to private net'
+ufw enable
+ufw status verbose
+
 # sync everything to disk
 sync
 
