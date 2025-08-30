783f6e4e3602293b2711178edf721ba04cb20c83
johndeedly <johndeedly@github.com>
2025-08-30 17:33:24 +0200

    use fixed numbers in proxmox devops build

--- config/apt/40_proxmox_devops_apt.sh
+++ config/apt/40_proxmox_devops_apt.sh
@@ -21,7 +21,6 @@ mkdir -p output
 env "HOME=$BUILDDIR/home" PACKER_LOG=1 PACKER_LOG_PATH=output/devops-linux.log \
     /bin/packer init devops-linux.pkr.hcl
 _cpu_cores=$(grep '^core id' /proc/cpuinfo | sort -u | wc -l)
-_idnum=$((1000))
 
 # debian router
 yq -y '(.setup.distro) = "debian"' config/setup.yml | sponge config/setup.yml
@@ -35,12 +34,11 @@ env "HOME=$BUILDDIR/home" PACKER_LOG=1 PACKER_LOG_PATH=output/devops-linux.log \
     /bin/packer build -force -only=qemu.default devops-linux.pkr.hcl
 if [ -f output/artifacts/tar/devops-linux-debian.tar.zst ]; then
   mv output/artifacts/tar/devops-linux-debian.tar.zst /var/lib/vz/template/cache/debian-x86_64-router.tar.zst
-  pct create "${_idnum}" /var/lib/vz/template/cache/debian-x86_64-router.tar.zst --ignore-unpack-errors 1 --memory 1536 \
+  pct create 400 /var/lib/vz/template/cache/debian-x86_64-router.tar.zst --ignore-unpack-errors 1 --memory 1536 \
     --hostname debian-router --storage local --swap 512 --rootfs local:64 \
     --net0 name=eth0,bridge=vmbr0,firewall=0,ip=dhcp,ip6=dhcp \
     --net1 name=eth1,bridge=vmbrlan0,firewall=0,ip=manual,ip6=manual \
     --unprivileged 1 --pool pool0 --ostype debian --onboot 1 --features nesting=1 --protection 1
-  _idnum=$((_idnum + 1))
 fi
 
 # archlinux mirror server
@@ -55,11 +53,10 @@ env "HOME=$BUILDDIR/home" PACKER_LOG=1 PACKER_LOG_PATH=output/devops-linux.log \
     /bin/packer build -force -only=qemu.default devops-linux.pkr.hcl
 if [ -f output/artifacts/tar/devops-linux-archlinux.tar.zst ]; then
   mv output/artifacts/tar/devops-linux-archlinux.tar.zst /var/lib/vz/template/cache/archlinux-x86_64-mirror.tar.zst
-  pct create "${_idnum}" /var/lib/vz/template/cache/archlinux-x86_64-mirror.tar.zst --ignore-unpack-errors 1 --memory 1536 \
+  pct create 401 /var/lib/vz/template/cache/archlinux-x86_64-mirror.tar.zst --ignore-unpack-errors 1 --memory 1536 \
     --hostname arch-mirror --storage local --swap 512 --rootfs local:512 \
     --net0 name=eth0,bridge=vmbrlan0,firewall=0,ip=dhcp,ip6=dhcp \
     --unprivileged 1 --pool pool0 --ostype archlinux --onboot 1 --features nesting=1 --protection 1
-  _idnum=$((_idnum + 1))
 fi
 
 # debian 13 mirror server
@@ -74,11 +71,13 @@ env "HOME=$BUILDDIR/home" PACKER_LOG=1 PACKER_LOG_PATH=output/devops-linux.log \
     /bin/packer build -force -only=qemu.default devops-linux.pkr.hcl
 if [ -f output/artifacts/tar/devops-linux-debian-13.tar.zst ]; then
   mv output/artifacts/tar/devops-linux-debian-13.tar.zst /var/lib/vz/template/cache/debian-13-x86_64-mirror.tar.zst
-  pct create "${_idnum}" /var/lib/vz/template/cache/debian-13-x86_64-mirror.tar.zst --ignore-unpack-errors 1 --memory 1536 \
+  pct create 402 /var/lib/vz/template/cache/debian-13-x86_64-mirror.tar.zst --ignore-unpack-errors 1 --memory 1536 \
     --hostname debian-13-mirror --storage local --swap 512 --rootfs local:512 \
     --net0 name=eth0,bridge=vmbrlan0,firewall=0,ip=dhcp,ip6=dhcp \
     --unprivileged 1 --pool pool0 --ostype debian --onboot 1 --features nesting=1 --protection 1
-  _idnum=$((_idnum + 1))
+else
+  echo "Files in output/artifacts/tar/:"
+  ls -la output/artifacts/tar
 fi
 
 # exit build environment
