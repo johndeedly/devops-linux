b51288de464746b2a76d2916a1bd3065625079fd
johndeedly <johndeedly@github.com>
2025-08-31 08:52:32 +0200

    verify tar image and rebuild on extraction errors

--- config/tar/90_tar_image.sh
+++ config/tar/90_tar_image.sh
@@ -5,9 +5,20 @@ exec &> >(while IFS=$'\r' read -ra line; do [ -z "${line[@]}" ] && line=( '' ); 
 # https://wiki.archlinux.org/title/Full_system_backup_with_tar
 DISTRO_NAME=$(yq -r '.setup.distro' /var/lib/cloud/instance/config/setup.yml)
 mkdir -p /srv/tar
-ZSTD_CLEVEL=4 ZSTD_NBTHREADS=4 tar -I zstd \
-  --exclude="./boot/efi" --exclude="./cidata*" --exclude="./dev/*" --exclude="./efi" --exclude="./etc/fstab*" --exclude="./etc/crypttab*" --exclude="./etc/systemd/system/cloud-*" --exclude="./usr/lib/systemd/system/cloud-*" --exclude="./proc/*" --exclude="./sys/*" --exclude="./run/*" --exclude="./mnt/*" --exclude="./share/*" --exclude="./srv/pxe" --exclude="./srv/img" --exclude="./srv/tar" --exclude="./media/*" --exclude="./tmp/*" --exclude="./swap/*" --exclude="./usr/lib/firmware/*" --exclude="./var/tmp/*" --exclude="./var/log/*" --exclude="./var/cache/pacman/pkg/*" --exclude="./var/cache/apt/*" --exclude="./var/lib/cloud/*" \
-  --acls --xattrs -cpaf "/srv/tar/devops-linux-${DISTRO_NAME}.tar.zst" -C / .
+echo "[ ## ] Create tar image"
+checked=0
+while [ "$checked" -eq 0 ]; do
+  ZSTD_CLEVEL=4 ZSTD_NBTHREADS=4 tar -I zstd \
+    --exclude="./boot/efi/*" --exclude="./cidata*" --exclude="./dev/*" --exclude="./efi/*" --exclude="./etc/fstab*" --exclude="./etc/crypttab*" --exclude="./etc/systemd/system/cloud-*" --exclude="./usr/lib/systemd/system/cloud-*" --exclude="./proc/*" --exclude="./sys/*" --exclude="./run/*" --exclude="./mnt/*" --exclude="./share/*" --exclude="./srv/pxe/*" --exclude="./srv/img/*" --exclude="./srv/tar/*" --exclude="./media/*" --exclude="./tmp/*" --exclude="./swap/*" --exclude="./usr/lib/firmware/*" --exclude="./var/tmp/*" --exclude="./var/log/*" --exclude="./var/cache/pacman/pkg/*" --exclude="./var/cache/apt/*" --exclude="./var/lib/cloud/*" \
+    -cf "/srv/tar/devops-linux-${DISTRO_NAME}.tar.zst" -C / .
+  echo "[ ## ] Verify tar image"
+  if ! ZSTD_CLEVEL=4 ZSTD_NBTHREADS=4 tar -I zstd -xOf "/srv/tar/devops-linux-${DISTRO_NAME}.tar.zst" &> /dev/null; then
+    echo "[FAIL] Broken tar image, retry"
+  else
+    echo "[ OK ] Valid image created"
+    checked=1
+  fi
+done
 
 # sync everything to disk
 sync
