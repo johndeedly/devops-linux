694effd63fe72ec3673bb05fa56583a502034168
johndeedly <johndeedly@github.com>
2025-08-31 10:36:01 +0200

    protect the system from a breaking tar process

--- config/tar/95_tar_image.sh
+++ config/tar/95_tar_image.sh
@@ -8,12 +8,15 @@ mkdir -p /srv/tar
 echo "[ ## ] Create tar image"
 checked=0
 while [ "$checked" -eq 0 ]; do
-  ZSTD_CLEVEL=4 ZSTD_NBTHREADS=4 tar -I zstd \
+  ( ZSTD_CLEVEL=4 ZSTD_NBTHREADS=4 tar -I zstd \
     --exclude="./boot/efi/*" --exclude="./cidata*" --exclude="./dev/*" --exclude="./efi/*" --exclude="./etc/fstab*" --exclude="./etc/crypttab*" --exclude="./etc/systemd/system/cloud-*" --exclude="./usr/lib/systemd/system/cloud-*" --exclude="./proc/*" --exclude="./sys/*" --exclude="./run/*" --exclude="./mnt/*" --exclude="./share/*" --exclude="./srv/pxe/*" --exclude="./srv/img/*" --exclude="./srv/tar/*" --exclude="./media/*" --exclude="./tmp/*" --exclude="./swap/*" --exclude="./usr/lib/firmware/*" --exclude="./var/tmp/*" --exclude="./var/log/*" --exclude="./var/cache/pacman/pkg/*" --exclude="./var/cache/apt/*" --exclude="./var/lib/cloud/*" \
-    -cf "/srv/tar/devops-linux-${DISTRO_NAME}.tar.zst" -C / .
+    -cf "/srv/tar/devops-linux-${DISTRO_NAME}.tar.zst" -C / . ) &
+  pid=$!
+  wait $pid
   echo "[ ## ] Verify tar image"
   if ! ZSTD_CLEVEL=4 ZSTD_NBTHREADS=4 tar -I zstd -xOf "/srv/tar/devops-linux-${DISTRO_NAME}.tar.zst" &> /dev/null; then
     echo "[FAIL] Broken tar image, retry"
+    [ -f "/srv/tar/devops-linux-${DISTRO_NAME}.tar.zst" ] && rm "/srv/tar/devops-linux-${DISTRO_NAME}.tar.zst"
   else
     echo "[ OK ] Valid image created"
     checked=1
