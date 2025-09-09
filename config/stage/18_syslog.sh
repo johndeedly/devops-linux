47ddb1f5adb8b6f48f24f15b5e3af00dfdf1688b
johndeedly <johndeedly@github.com>
2025-09-09 19:06:30 +0200

    RFC 5424 syslog server with optional certificate authentication

--- config/stage/18_syslog.sh
+++ config/stage/18_syslog.sh
@@ -5,6 +5,10 @@ exec &> >(while IFS=$'\r' read -ra line; do [ -z "${line[@]}" ] && line=( '' ); 
 SYSLOG_ENABLED="$(yq -r '.setup.remote_log.enabled' /var/lib/cloud/instance/config/setup.yml)"
 SYSLOG_SERVER="$(yq -r '.setup.remote_log.syslog_server' /var/lib/cloud/instance/config/setup.yml)"
 SYSLOG_PORT="$(yq -r '.setup.remote_log.syslog_port' /var/lib/cloud/instance/config/setup.yml)"
+SYSLOG_X509_KEY="$(yq -r '.setup.remote_log.x509_key' /var/lib/cloud/instance/config/setup.yml)"
+SYSLOG_X509_CRT="$(yq -r '.setup.remote_log.x509_crt' /var/lib/cloud/instance/config/setup.yml)"
+SYSLOG_X509_HASH="$(yq -r '.setup.remote_log.x509_hash' /var/lib/cloud/instance/config/setup.yml)"
+SYSLOG_PEER_VERIFY="$(yq -r '.setup.remote_log.peer_verify' /var/lib/cloud/instance/config/setup.yml)"
 
 if [ -z "$SYSLOG_ENABLED" ] || [[ "$SYSLOG_ENABLED" =~ [Nn][Oo] ]] || [[ "$SYSLOG_ENABLED" =~ [Oo][Ff][Ff] ]] || [[ "$SYSLOG_ENABLED" =~ [Ff][Aa][Ll][Ss][Ee] ]]
 then
@@ -13,8 +17,39 @@ then
   exit 0
 fi
 
+if [ -n "${SYSLOG_X509_KEY}" ] && [ -n "${SYSLOG_X509_CRT}" ]; then
+  mkdir -p /etc/syslog-ng/cert.d
+  tee /etc/syslog-ng/cert.d/client.key <<EOF
+${SYSLOG_X509_KEY}
+EOF
+  tee /etc/syslog-ng/cert.d/client.crt <<EOF
+${SYSLOG_X509_CRT}
+EOF
+  chmod 0700 /etc/syslog-ng/cert.d
+  chmod 0600 /etc/syslog-ng/cert.d/client.{key,crt}
+fi
+
 # enable RFC 5424 logging
-tee -a /etc/syslog-ng/syslog-ng.conf <<EOF
+if [ -n "${SYSLOG_X509_KEY}" ] && [ -n "${SYSLOG_X509_CRT}" ]; then
+  tee -a /etc/syslog-ng/syslog-ng.conf <<EOF
+
+destination d_prov_net {
+  syslog("${SYSLOG_SERVER}" port(${SYSLOG_PORT}) transport(tls) tls(
+    peer-verify(${SYSLOG_PEER_VERIFY})
+    trusted-keys("${SYSLOG_X509_HASH}")
+    key-file("/etc/syslog-ng/cert.d/client.key")
+    cert-file("/etc/syslog-ng/cert.d/client.crt")
+  ));
+};
+
+log {
+  source(s_prov_system);
+  filter(f_prov_system);
+  destination(d_prov_net);
+};
+EOF
+else
+  tee -a /etc/syslog-ng/syslog-ng.conf <<EOF
 
 destination d_prov_net {
   syslog("${SYSLOG_SERVER}" port(${SYSLOG_PORT}));
@@ -26,6 +61,7 @@ log {
   destination(d_prov_net);
 };
 EOF
+fi
 
 # sync everything to disk
 sync
--- config/stage/18_syslog_server.sh
+++ config/stage/18_syslog_server.sh
@@ -6,6 +6,10 @@ SYSLOG_SERVER_ENABLED="$(yq -r '.setup.logserver.enabled' /var/lib/cloud/instanc
 SYSLOG_LOGFILE="$(yq -r '.setup.logserver.logfile' /var/lib/cloud/instance/config/setup.yml)"
 SYSLOG_BIND_IP="$(yq -r '.setup.logserver.bind_ip' /var/lib/cloud/instance/config/setup.yml)"
 SYSLOG_BIND_PORT="$(yq -r '.setup.logserver.bind_port' /var/lib/cloud/instance/config/setup.yml)"
+SYSLOG_X509_KEY="$(yq -r '.setup.remote_log.x509_key' /var/lib/cloud/instance/config/setup.yml)"
+SYSLOG_X509_CRT="$(yq -r '.setup.remote_log.x509_crt' /var/lib/cloud/instance/config/setup.yml)"
+SYSLOG_X509_HASH="$(yq -r '.setup.remote_log.x509_hash' /var/lib/cloud/instance/config/setup.yml)"
+SYSLOG_PEER_VERIFY="$(yq -r '.setup.remote_log.peer_verify' /var/lib/cloud/instance/config/setup.yml)"
 
 if [ -z "$SYSLOG_SERVER_ENABLED" ] || [[ "$SYSLOG_SERVER_ENABLED" =~ [Nn][Oo] ]] || [[ "$SYSLOG_SERVER_ENABLED" =~ [Oo][Ff][Ff] ]] || [[ "$SYSLOG_SERVER_ENABLED" =~ [Ff][Aa][Ll][Ss][Ee] ]]
 then
@@ -14,8 +18,42 @@ then
   exit 0
 fi
 
+if [ -n "${SYSLOG_X509_KEY}" ] && [ -n "${SYSLOG_X509_CRT}" ]; then
+  mkdir -p /etc/syslog-ng/cert.d
+  tee /etc/syslog-ng/cert.d/client.key <<EOF
+${SYSLOG_X509_KEY}
+EOF
+  tee /etc/syslog-ng/cert.d/client.crt <<EOF
+${SYSLOG_X509_CRT}
+EOF
+  chmod 0700 /etc/syslog-ng/cert.d
+  chmod 0600 /etc/syslog-ng/cert.d/client.{key,crt}
+fi
+
 # RFC 5424 logging from remote clients
-tee -a /etc/syslog-ng/syslog-ng.conf <<EOF
+if [ -n "${SYSLOG_X509_KEY}" ] && [ -n "${SYSLOG_X509_CRT}" ]; then
+  tee -a /etc/syslog-ng/syslog-ng.conf <<EOF
+
+source s_prov_remote {
+  syslog(ip("${SYSLOG_BIND_IP}") port(${SYSLOG_BIND_PORT}) transport(tls) tls(
+    peer-verify(${SYSLOG_PEER_VERIFY})
+    trusted-keys("${SYSLOG_X509_HASH}")
+    key-file("/etc/syslog-ng/cert.d/client.key")
+    cert-file("/etc/syslog-ng/cert.d/client.crt")
+  ));
+};
+
+destination d_prov_remote {
+  file("${SYSLOG_LOGFILE}");
+};
+
+log {
+  source(s_prov_remote);
+  destination(d_prov_remote);
+};
+EOF
+else
+  tee -a /etc/syslog-ng/syslog-ng.conf <<EOF
 
 source s_prov_remote {
   syslog(ip("${SYSLOG_BIND_IP}") port(${SYSLOG_BIND_PORT}));
@@ -30,6 +68,7 @@ log {
   destination(d_prov_remote);
 };
 EOF
+fi
 
 ufw disable
 if [ -z "${SYSLOG_BIND_IP}" ] || [ "0.0.0.0" = "${SYSLOG_BIND_IP}" ] || [ "::" = "${SYSLOG_BIND_IP}" ]; then
