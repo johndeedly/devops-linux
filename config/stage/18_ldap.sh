f71da8c0267d1dca94c477949cc208c0dbe5b873
johndeedly <johndeedly@github.com>
2025-09-09 08:21:23 +0200

    bugfix: ubuntu does ldap auth different than all the others

--- config/stage/18_ldap.sh
+++ config/stage/18_ldap.sh
@@ -19,44 +19,65 @@ fi
 # install basic packages
 if [ -e /bin/apt ]; then
   LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata apt -y install \
-    libnss-ldap libpam-ldap ldap-utils nslcd xkcdpass
+    libnss-ldap libpam-ldap ldap-utils nslcd
 elif [ -e /bin/pacman ]; then
   LC_ALL=C yes | LC_ALL=C pacman -S --noconfirm --needed \
-    openldap nss-pam-ldapd xkcdpass
+    openldap nss-pam-ldapd
 elif [ -e /bin/yum ]; then
   LC_ALL=C yes | LC_ALL=C yum install -y \
-    openldap openldap-clients nss-pam-ldapd xkcdpass
+    openldap openldap-clients nss-pam-ldapd
 fi
 
-echo ':: enable optional ldap pam and nss authentication'
+# optional ldap pam and nss authentication
 if [ -f /etc/pam.d/system-auth ]; then
-    sed -i '0,/^auth/s//auth       sufficient                  pam_ldap.so\nauth/' /etc/pam.d/system-auth
-    sed -i '0,/^account/s//account    sufficient                  pam_ldap.so\naccount/' /etc/pam.d/system-auth
-    sed -i '0,/^password/s//password   sufficient                  pam_ldap.so\npassword/' /etc/pam.d/system-auth
-    sed -i '0,/^session/s//session    optional                    pam_ldap.so\nsession/' /etc/pam.d/system-auth
+    sed -i '0,/^\(auth.*pam_unix.so.*\)/s//auth       sufficient                  pam_ldap.so   minimum_uid=1000\n\1/' /etc/pam.d/system-auth
+    sed -i '0,/^\(account.*pam_unix.so.*\)/s//account    sufficient                  pam_ldap.so   minimum_uid=1000\n\1/' /etc/pam.d/system-auth
+    sed -i '0,/^\(password.*pam_unix.so.*\)/s//password   sufficient                  pam_ldap.so   minimum_uid=1000\n\1/' /etc/pam.d/system-auth
+    sed -i '0,/^\(session.*pam_unix.so.*\)/s//\1\nsession    optional                    pam_ldap.so   minimum_uid=1000/' /etc/pam.d/system-auth
+fi
+if [ -f /etc/pam.d/system-login ]; then
+    sed -i '0,/^\(session.*pam_env.so.*\)/s||\1\nsession    required   pam_mkhomedir.so   skel=/etc/skel umask=0077|' /etc/pam.d/system-login
 fi
 if [ -f /etc/pam.d/su ]; then
-    sed -i '0,/pam_rootok.so/s//pam_rootok.so\nauth            sufficient      pam_ldap.so/' /etc/pam.d/su
-    sed -i 's/^\(auth.*pam_unix.so\)/\1 use_first_pass/' /etc/pam.d/su
-    sed -i '0,/^account/s//account         sufficient      pam_ldap.so\naccount/' /etc/pam.d/su
-    sed -i '0,/^session/s//session         sufficient      pam_ldap.so\nsession/' /etc/pam.d/su
+    sed -i '0,/^\(.*pam_rootok.so.*\)/s//\1\nauth            sufficient      pam_ldap.so/' /etc/pam.d/su
+    sed -i 's/^\(auth.*pam_unix.so\)/\1   use_first_pass/' /etc/pam.d/su
+    sed -i '0,/^\(account.*pam_unix.so.*\)/s//account         sufficient      pam_ldap.so\n\1/' /etc/pam.d/su
+    sed -i '0,/^\(session.*pam_unix.so.*\)/s//session         sufficient      pam_ldap.so\n\1/' /etc/pam.d/su
 fi
 if [ -f /etc/pam.d/su-l ]; then
-    sed -i '0,/pam_rootok.so/s//pam_rootok.so\nauth            sufficient      pam_ldap.so/' /etc/pam.d/su-l
-    sed -i 's/^\(auth.*pam_unix.so\)/\1 use_first_pass/' /etc/pam.d/su-l
-    sed -i '0,/^account/s//account         sufficient      pam_ldap.so\naccount/' /etc/pam.d/su-l
-    sed -i '0,/^session/s//session         sufficient      pam_ldap.so\nsession/' /etc/pam.d/su-l
+    sed -i '0,/^\(.*pam_rootok.so.*\)/s//\1\nauth            sufficient      pam_ldap.so/' /etc/pam.d/su-l
+    sed -i 's/^\(auth.*pam_unix.so\)/\1   use_first_pass/' /etc/pam.d/su-l
+    sed -i '0,/^\(account.*pam_unix.so.*\)/s//account         sufficient      pam_ldap.so\n\1/' /etc/pam.d/su-l
+    sed -i '0,/^\(session.*pam_unix.so.*\)/s//session         sufficient      pam_ldap.so\n\1/' /etc/pam.d/su-l
+    sed -i '0,/^\(session.*pam_ldap.so.*\)/s||session         required        pam_mkhomedir.so   skel=/etc/skel umask=0077\n\1|' /etc/pam.d/su-l
 fi
+if [ -f /etc/pam.d/sudo ]; then
+    sed -i 's/^\(auth.*pam_unix.so\)/auth      sufficient    pam_ldap.so\n\1 try_first_pass/' /etc/pam.d/sudo
+fi
+
+# link passwd and group lists with ldap
 if [ -f /etc/nslcd.conf ]; then
   chmod 0600 /etc/nslcd.conf
   sed -i "s|^\(uri .*\)|#\1\nuri ${LDAP_AUTHSERVER}|" /etc/nslcd.conf
   sed -i "s|^\(base .*\)|#\1\nbase   ${LDAP_BASE}\nbase   group  ${LDAP_GROUP}\nbase   passwd ${LDAP_PASSWD}\nbase   shadow ${LDAP_SHADOW}|" /etc/nslcd.conf
+  sed -i "s/^ldap_version/#ldap_version/" /etc/nslcd.conf
 fi
+
+# thanks, ubuntu, for the inconsistency you provide every time
+# debian uses default confs, ubuntu doesn't
+if [ -f /etc/ldap.conf ]; then
+  sed -i "s|^\(uri .*\)|#\1\nuri ${LDAP_AUTHSERVER}|" /etc/ldap.conf
+  sed -i "s|^\(base .*\)|#\1\nbase   ${LDAP_BASE}|" /etc/ldap.conf
+fi
+
+# prioritize ldap to be after everything else
 if [ -f /etc/nsswitch.conf ]; then
     sed -i 's/^\(passwd.*\)/\1 ldap/' /etc/nsswitch.conf
     sed -i 's/^\(group.*\)/\1 ldap/' /etc/nsswitch.conf
     sed -i 's/^\(shadow.*\)/\1 ldap/' /etc/nsswitch.conf
 fi
+
+# configure ldap client connection
 if [ -f /etc/openldap/ldap.conf ]; then
   tee -a /etc/openldap/ldap.conf <<EOF
 
@@ -64,8 +85,12 @@ BASE    ${LDAP_BASE}
 URI     ${LDAP_AUTHSERVER}
 EOF
 fi
-if [ -f /etc/pam.d/sudo ]; then
-    sed -i 's/^\(auth.*pam_unix.so\)/auth      sufficient    pam_ldap.so\n\1 try_first_pass/' /etc/pam.d/sudo
+if [ -f /etc/ldap/ldap.conf ]; then
+  tee -a /etc/ldap/ldap.conf <<EOF
+
+BASE    ${LDAP_BASE}
+URI     ${LDAP_AUTHSERVER}
+EOF
 fi
 
 # Enable all configured services
