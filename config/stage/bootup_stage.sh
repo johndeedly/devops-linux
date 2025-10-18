47311a4744ea3f39697925f845be6ed82d36841f
johndeedly <johndeedly@github.com>
2025-10-18 07:42:15 +0200

    permanent hardening of ssh connections to the image

--- config/stage/bootup_stage.sh
+++ config/stage/bootup_stage.sh
@@ -9,9 +9,13 @@ if [ -f /usr/lib/systemd/system/systemd-time-wait-sync.service ]; then
 fi
 
 # enable ssh provision login -> disable root, allow provisioning account, password auth, use pam
-sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
-sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^#\?UsePAM.*/UsePAM yes/' /etc/ssh/sshd_config
+sed -i 's/^#\? \?PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
+sed -i 's/^#\? \?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/^#\? \?AllowTcpForwarding.*/AllowTcpForwarding no/' /etc/ssh/sshd_config
+sed -i 's/^#\? \?AllowAgentForwarding.*/AllowAgentForwarding no/' /etc/ssh/sshd_config
+sed -i 's/^#\? \?X11Forwarding.*/X11Forwarding no/' /etc/ssh/sshd_config
+sed -i 's/^#\? \?TCPKeepAlive.*/TCPKeepAlive no/' /etc/ssh/sshd_config
+sed -i 's/^#\? \?UsePAM.*/UsePAM yes/' /etc/ssh/sshd_config
 # remove all occurences of Match User root until the next empty line
 sed -i '/^Match User root/,/^$/d' /etc/ssh/sshd_config
 # remove empty lines at end of file
