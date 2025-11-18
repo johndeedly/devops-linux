f056d0ea7f52d4bb633d16aed85dc9380adfae0a
johndeedly <johndeedly@github.com>
2025-11-18 20:53:25 +0100

    proxmox cluster worker joins on next boot

--- config/apt/30_proxmox_worker_2_apt.sh
+++ config/apt/30_proxmox_worker_2_apt.sh
@@ -12,7 +12,6 @@ LC_ALL=C yes | LC_ALL=C DEBIAN_FRONTEND=noninteractive eatmydata apt install soc
 # get cluster key
 PROXMOX_CLUSTER_KEY="$(yq -r '.setup.proxmox_cluster.cluster_key' /var/lib/cloud/instance/config/setup.yml)"
 PROXMOX_CLUSTER_PUB="$(yq -r '.setup.proxmox_cluster.cluster_pub' /var/lib/cloud/instance/config/setup.yml)"
-PROXMOX_MASTER_FQDN_OR_IP="$(yq -r '.setup.proxmox_cluster.master_fqdn_or_ip' /var/lib/cloud/instance/config/setup.yml)"
 
 # enable cluster key
 # https://forum.proxmox.com/threads/etc-pve-priv-authorized_keys.18561/
@@ -30,17 +29,23 @@ Type=_proxmoxcluster._tcp
 SubType=_worker
 EOF
 
+# try joining the cluster on next boot
+tee /usr/local/bin/join-the-pve-cluster.sh <<EOF
+#!/usr/bin/env bash
+PROXMOX_MASTER_FQDN_OR_IP="$(yq -r '.setup.proxmox_cluster.master_fqdn_or_ip' /var/lib/cloud/instance/config/setup.yml)"
+PROXMOX_CEPH_OSD_DEVICE="$(yq -r '.setup.proxmox_cluster.ceph_osd_device' /var/lib/cloud/instance/config/setup.yml)"
+
 # broadcast for master
-until [ -n "${PROXMOX_MASTER_FQDN_OR_IP}" ]; do
+until [ -n "\${PROXMOX_MASTER_FQDN_OR_IP}" ]; do
   echo "[ ## ] Broadcasting to master node..."
-  PROXMOX_MASTER_NAME=$(resolvectl query -p mdns --type=PTR --zone=no --json=short _master._sub._proxmoxcluster._tcp.local 2>/dev/null | jq -r '.name')
-  PROXMOX_MASTER_FQDN_OR_IP=$(resolvectl query -p mdns --type=SRV --zone=no --json=short "${PROXMOX_MASTER_NAME}" 2>/dev/null | jq -r '.name')
+  PROXMOX_MASTER_NAME=\$(resolvectl query -p mdns --type=PTR --zone=no --json=short _master._sub._proxmoxcluster._tcp.local 2>/dev/null | jq -r '.name')
+  PROXMOX_MASTER_FQDN_OR_IP=\$(resolvectl query -p mdns --type=SRV --zone=no --json=short "\${PROXMOX_MASTER_NAME}" 2>/dev/null | jq -r '.name')
 done
-echo "[ ## ] Master node configured: ${PROXMOX_MASTER_FQDN_OR_IP}"
-ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=accept-new root@"${PROXMOX_MASTER_FQDN_OR_IP}" 'exit'
+echo "[ ## ] Master node configured: \${PROXMOX_MASTER_FQDN_OR_IP}"
+ssh -i /root/.ssh/id_ed25519 -o StrictHostKeyChecking=accept-new root@"\${PROXMOX_MASTER_FQDN_OR_IP}" 'exit'
 
 # join the cluster
-pvecm add "${PROXMOX_MASTER_FQDN_OR_IP}" --use_ssh true
+pvecm add "\${PROXMOX_MASTER_FQDN_OR_IP}" --use_ssh true
 pvecm updatecerts --silent true
 pvecm status
 pvecm nodes
@@ -48,11 +53,29 @@ pvecm nodes
 # configure ceph
 pveceph mgr create
 pveceph mon create
-PROXMOX_CEPH_OSD_DEVICE="$(yq -r '.setup.proxmox_cluster.ceph_osd_device' /var/lib/cloud/instance/config/setup.yml)"
-if [ -n "$PROXMOX_CEPH_OSD_DEVICE" ]; then
-  pveceph osd create "$PROXMOX_CEPH_OSD_DEVICE"
+if [ -n "\$PROXMOX_CEPH_OSD_DEVICE" ]; then
+  pveceph osd create "\$PROXMOX_CEPH_OSD_DEVICE"
 fi
 
+systemctl disable join-the-pve-cluster
+EOF
+chmod +x /usr/local/bin/join-the-pve-cluster.sh
+tee /etc/systemd/system/join-the-pve-cluster.service <<EOF
+[Unit]
+Description=Joins a Proxmox cluster on startup
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+StandardInput=null
+StandardOutput=journal
+StandardError=journal
+ExecStart=/usr/local/bin/join-the-pve-cluster.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable join-the-pve-cluster
 
 # sync everything to disk
 sync
